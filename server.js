0f8e67c8a960a3b041f82096a1c875322b2ec26d
송지훈 <songjihun@songjihun-ui-MacBookPro.local>
2021-11-17 20:10:21 +0900

    sqlite 데이터베이스 통신 및 상품업로드 기능 구현

---
+++ server.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const models = require('./models');
+const multer = require('multer');
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
+const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  '/uploads',
+  express.static(
+    'uploads'
+  ) /* 서버에서는 uploads 안에 있는 파일들을 입력했던 경로와 다른 경로(크롬 사용자설정에 있는 경로)로 보여줘야하기 때문에
+  이미지를 받아서 해당 imageurl에 맞춰서 node 서버에 있는 이미지를 불러왔다.*/
+);
+
+app.get('/products', (req, res) => {
+  models.Product.findAll({
+    order: [['createdAt', 'DESC']],
+    attributes: ['id', 'name', 'price', 'createdAt', 'seller', 'imageUrl'],
+  })
+    .then((result) => {
+      console.log('PRODUCTS : ', result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send('에러 발생');
+    });
+});
+
+app.post('/products', (req, res) => {
+  const body = req.body;
+  const { name, description, price, seller } = body;
+  if (!name || !description || !price || !seller) {
+    res.send('모든 필드를 입력해주세요');
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then((result) => {
+      console.log('상품 생성 결과 : ', result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send('상품 업로드에 문제가 발생했습니다');
+    });
+});
+
+app.get('/products/:id', (req, res) => {
+  const params = req.params;
+  const { id } = params;
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log('PRODUCT : ', result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send('상품 조회에 에러가 발생했습니다');
+    });
+});
+
+app.post('/image', upload.single('image'), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
+});
+
+app.listen(port, () => {
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log('DB 연결 성공!');
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log('DB 연결 에러ㅠ');
+      process.exit();
+    });
+});
